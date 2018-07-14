03a04b910ac60985f54f4ca6deb369bb04f28b81
Enzo Lizama <lizama.enzo@gmail.com>
2018-07-14 14:33:56 -0500

    Start again with nodegit add .

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
-const mongoose = require('mongoose');
-const assert = require('assert');
+const minimist = require('minimist');
 
-mongoose.Promise = global.Promise; // Allow native promises without throwing error
-mongoose.connect('mongodb://localhost:27017/contact-manager');
-const db = mongoose.connection;
-
-function toLower(v) {
-    return v.toLowerCase()
+ module.exports =  () => {
+    const args = minimist(process.argv.slice(2));
+    console.log(args);
 }
-
-const contactSchema = mongoose.Schema({
-    firstName: { type: String , set: toLower},
-    lastName: { type: String , set: toLower},
-    phone: { type: String , set: toLower},
-    email: { type: String , set: toLower}
-});
-
-const Contact = mongoose.model('Contact',contactSchema);
-
-const addContact = (contact) => {
-    Contact.create(contact,(err) => {
-        assert.equal(null,err);
-        console.info('New contact added');
-        console.info(db.listCollections())
-        db.close();
-    });
-};
-
-const getContact = (name) =>{
-    const search = new RegExp(`^${name}$`,'i');
-    Contact.find({$or: [{firstName: search}, {lastName: search}]})
-        .exec((err,contact) => {
-            assert.equal(null,err);
-            console.info(contact);
-            console.info(`${contact.length} matches`);
-            db.close();
-    });
-};
-
-
-//Export methods
-module.exports = { addContact , getContact };
-
-
