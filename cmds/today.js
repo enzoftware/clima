09b976daa47d8c11f20a700abdb0c5c991b0021e
Enzo Lizama <lizama.enzo@gmail.com>
2018-07-15 23:08:14 -0500

    :rainbow: :rainbow: :rainbow:

--- cmds/today.js
+++ cmds/today.js
@@ -1,3 +1,20 @@
-module.exports = (args) => {
-    console.log("today is sunny");
+const ora = require('ora')
+const getWeather = require('../utils/weather')
+
+module.exports = async (args) => {
+  const spinner = ora().start()
+
+  try {
+    const location = args.location || args.l
+    const weather = await getWeather(location)
+
+    spinner.stop()
+
+    console.log(`Current conditions in ${location}:`)
+    console.log(`\t${weather.condition.temp}° ${weather.condition.text}`)
+  } catch (err) {
+    spinner.stop()
+
+    console.error(err)
+  }
 }
