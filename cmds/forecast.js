3265927f5eb69591ad9e5650129f5813cc09bc08
Enzo Lizama <lizama.enzo@gmail.com>
2018-07-15 21:44:20 -0500

    axios fetching :rainbow:

---
+++ cmds/forecast.js
@@ -0,0 +1,3 @@
+module.exports = (args) => {
+    console.log('tomorrow is rainy');
+}
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const minimist = require('minimist');
 
- module.exports =  () => {
+module.exports = () => {
     const args = minimist(process.argv.slice(2));
     let cmd = args._[0] || 'help';
 
-    if(args.version || args.v ){
+    if (args.version || args.v) {
         cmd = 'version';
     }
 
-    if(args.help || args.h){
+    if (args.help || args.h) {
         cmd = 'help';
     }
 
-    switch(cmd){
+    switch (cmd) {
         case 'today':
             require('./cmds/today')(args);
             break;
@@ -22,7 +22,10 @@ const minimist = require('minimist');
         case 'help':
             require('./cmds/help')(args);
             break;
-        default :
+        case 'forecast':
+            require('./cmds/forecast')(args);
+            break;
+        default:
             console.error(`"${cmd}" is not a valid command!`);
             break;
     }
