5e3fca41f3e2c14dbc956b2b6904fcaf082a4e0d
Enzo Lizama <lizama.enzo@gmail.com>
2018-07-15 21:16:51 -0500

    cmds commands :rainbow: :rainbow:

--- index.js
+++ index.js
@@ -2,5 +2,28 @@ const minimist = require('minimist');
 
  module.exports =  () => {
     const args = minimist(process.argv.slice(2));
-    console.log(args);
+    let cmd = args._[0] || 'help';
+
+    if(args.version || args.v ){
+        cmd = 'version';
+    }
+
+    if(args.help || args.h){
+        cmd = 'help';
+    }
+
+    switch(cmd){
+        case 'today':
+            require('./cmds/today')(args);
+            break;
+        case 'version':
+            require('./cmds/version')(args);
+            break;
+        case 'help':
+            require('./cmds/help')(args);
+            break;
+        default :
+            console.error(`"${cmd}" is not a valid command!`);
+            break;
+    }
 }
